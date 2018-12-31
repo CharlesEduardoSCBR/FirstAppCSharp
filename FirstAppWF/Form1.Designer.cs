a4478e3c851462c4ccbe5e6cb5d8261a96c76464
Charles Eduardo <charles.eduardo.sc.br@gmail.com>
2018-12-31 14:48:35 -0200

    First project

---
+++ FirstAppWF/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FirstAppWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clickMe
+            // 
+            this.clickMe.Location = new System.Drawing.Point(84, 49);
+            this.clickMe.Name = "clickMe";
+            this.clickMe.Size = new System.Drawing.Size(75, 23);
+            this.clickMe.TabIndex = 0;
+            this.clickMe.Text = "Click-me";
+            this.clickMe.UseVisualStyleBackColor = true;
+            this.clickMe.Click += new System.EventHandler(this.clickMe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 134);
+            this.Controls.Add(this.clickMe);
+            this.Name = "Form1";
+            this.Text = "FirstAppWF";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMe;
+    }
+}
+
